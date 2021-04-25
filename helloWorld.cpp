3d66fab0925ff45413b35185eab37c8b29554aa8
Ubuntu <ubuntu@ip-172-31-49-223.ec2.internal>
2021-04-25 00:29:14 +0000

    added helloWorld.cpp

---
+++ helloWorld.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Jenoe Balote All rights reserved
+//
+// Created by Jenoe Balote
+// Created on April 2021
+// This is a "Hello, World!" program with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout <<"Hello, World!"<< std::endl;
+}
